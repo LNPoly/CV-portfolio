e632cfc686383d71e4faac3c6fd22632dfe457c9
LNPoly <paula.dominguez2205@gmail.com>
2025-02-25 17:20:53 -0300

    changes in html, js and css files. New svg directory.

--- js/header.js
+++ js/header.js
@@ -5,12 +5,12 @@ navbar.className = 'navbar';
 const navList = document.createElement('ul')
 
 const items = [
-    { className: 'home', href: 'index.html', icon: '<i class="fa-solid fa-house"></i>' },
-    { href: 'pages/info.html', text: 'Datos Personales' },
-    { href: 'pages/education.html', text: 'Educación' },
-    { href: 'pages/knowledge.html', text: 'Conocimientos' },
-    { href: 'pages/projects.html', text: 'Proyectos' },
-    { href: 'pages/workExperience.html', text: 'Experiencia Laboral' }
+    { className: 'home', href: 'index.html', icon: '<i class="fa-solid fa-house"></i>', text: 'Inicio'},
+    { href: '/pages/info.html', text: 'Datos Personales' },
+    { href: '/pages/education.html', text: 'Educación' },
+    { href: '/pages/knowledge.html', text: 'Conocimientos' },
+    { href: '/pages/projects.html', text: 'Proyectos' },
+    { href: '/pages/workExperience.html', text: 'Experiencia Laboral' }
 ];
 
 items.forEach(item => {
@@ -32,13 +32,7 @@ items.forEach(item => {
     navList.appendChild(li);
 });
 
-// Agregar la lista a la navbar
 navbar.appendChild(navList);
-
-// Agregar la navbar al header
 header.appendChild(navbar);
 
 document.body.insertBefore(header, document.body.firstChild);
-
-// Finalmente, agregar el header al body o a otro contenedor
-//document.body.appendChild(header);
